127ca1589d215ddb116732adae8e4d295f9c9ee3
daxstrong <ilya.yurev.04@inbox.ru>
2023-09-11 22:56:32 +0300

    Вывод произведения

--- my_program.py
+++ my_program.py
@@ -2,4 +2,5 @@ num1 = float(input("Введите первое число: "))
 num2 = float(input("Введите второе число: "))
 result1 = num1 + num2
 result2 = num1 * num2
-print("Сумма чисел:", result)
+print("Сумма чисел:", result1)
+print("Произведение чисел:", result2)
